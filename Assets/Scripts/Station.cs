e6ea281652596bfd8d847a842d4435f8e76599bc
RDeBlasio <deblasio.richie@gmail.com>
2026-02-06 23:26:34 -0500

    feat: abstract station and enemy

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Station : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public abstract class Station: MonoBehaviour{
+
+    [SerializeField] private int maxHealth;
+
+    private int currentHealth;
+
+    [HideInInspector] public enum StationType
     {
-        
+        None,
+        HomeStation
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private StationType stationType;
+
+    protected virtual void Start()
     {
-        
+        currentHealth = maxHealth;
+
+        if (stationType == StationType.None)
+        {
+            Debug.LogError("You Didn't Set the Type Dummy!");
+        }
+    }
+    
+    public StationType getStationType()
+    {
+        return stationType;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public virtual void Die()
+    {
+        Destroy(gameObject);
     }
 }
