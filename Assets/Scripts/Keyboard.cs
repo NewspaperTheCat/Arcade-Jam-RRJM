04a48c759d4c48c2b6d11e3f26477e4fd8a5b507
RDeBlasio <deblasio.richie@gmail.com>
2026-02-08 01:49:59 -0500

    feat: keyboard shit

---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keyboard : MonoBehaviour
+{
+    [SerializeField] private int rows;
+    [SerializeField] private int cols;
+    [SerializeField] private int lastRowCols;
+
+
+    private char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', ',', '?', '!' };
+
+    private RectTransform container;
+
+    [SerializeField] private RectTransform prefab;
+
+    private List<RectTransform> keys = new();
+
+    private void Start()
+    {
+        container = GetComponent<RectTransform>();
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        float totalWidth = container.rect.width;
+        float totalHeight = container.rect.height;
+
+        int i = 0;
+
+        for (int r = 0; r < rows; r++)
+        {
+            float cellWidth = totalWidth / cols;
+            float cellHeight = totalHeight / rows;
+
+            for (int c = 0; c < cols; c++)
+            {
+                RectTransform key = Instantiate(prefab, container);
+                keys.Add(key);
+
+                float x = (c * cellWidth + cellWidth * 0.5f) - (container.rect.width / 2);
+                float y = -(r * cellHeight + cellHeight * 0.5f) + (container.rect.height / 2);
+
+                key.sizeDelta = new Vector2(cellWidth - 10, cellWidth - 10);
+
+                key.GetChild(0).GetComponent<TMP_Text>().text = letters[i].ToString();
+
+                key.anchoredPosition = new Vector2(x, y);
+
+                i++;
+
+            }
+        }
+    }
+}
